69f42a19583391a731c455fc26862474c9f5b81f
Yasaira Reynoso <reynoso.yasaira@student.greenriver.edu>
2022-02-08 17:22:15 -0500

    Add data layer

---
+++ model/data-layer.php
@@ -0,0 +1,15 @@
+<?php
+
+function getDessert(){
+    // Array Variables
+    $dessertsOption = array('chocolate'=>'Chocolate Mouse',
+        'vanilla'=>'Vanilla Custard',
+        'strawberry'=>'Strawberry Shortcake');
+    return $dessertsOption;
+}
+
+function getColors(){
+    // Array Variables
+    $colors = array('Green', 'Pink', 'Purple', 'Blue');
+    return $colors;
+}
